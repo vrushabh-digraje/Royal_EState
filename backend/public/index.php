7ab74dc47bb2c95cf94341849ae483f592ec76e2
Vrushabh Digraje <vrushabhdigraje07@gmail.com>
2026-01-31 18:29:45 +0530

    Modify CORS headers in index.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,11 +4,10 @@ error_reporting(0);
 
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
-    http_response_code(200);
     exit;
 }
 
